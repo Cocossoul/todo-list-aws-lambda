4aa6d7b1e30e8d17835542be68d8ec5b4d411134
Corentin Pape <corentin.pape@epita.fr>
2023-01-15 20:07:30 +0100

    fixup! feat(terraform): use s3 bucket instead of amplify

--- topology.tf
+++ topology.tf
@@ -11,27 +11,45 @@ provider "aws" {
   region = "eu-west-3"
 }
 
-variable "github_access_token" {
-  type    = string
+resource "aws_s3_bucket" "todo_list" {
+  bucket = "todo-list-www-bucket"
 }
 
-resource "aws_amplify_app" "todo_list" {
-  name       = "todo-list"
-  repository = "https://github.com/Cocossoul/todo-list-aws-lambda"
+resource "aws_s3_bucket_policy" "todo_list" {
+  bucket = aws_s3_bucket.todo_list.id
+  policy = templatefile("./www/policy.json", {
+    bucket_name = aws_s3_bucket.todo_list.bucket
+  })
+}
 
-  access_token = var.github_access_token
+output "website_address" {
+  value = aws_s3_bucket_website_configuration.todo_list.website_endpoint
+}
 
-  enable_branch_auto_build = true
+resource "aws_s3_bucket_acl" "todo_list" {
+  bucket = aws_s3_bucket.todo_list.id
+  acl    = "public-read"
+}
 
-  build_spec = file("amplify.yml")
-  environment_variables = {
-    AMPLIFY_MONOREPO_APP_ROOT = "www"
-    AMPLIFY_DIFF_DEPLOY = "false"
+resource "aws_s3_bucket_website_configuration" "todo_list" {
+  bucket = aws_s3_bucket.todo_list.bucket
+
+  index_document {
+    suffix = "index.html"
   }
 }
 
-resource "aws_amplify_branch" "todo_list_prod" {
-  app_id      = aws_amplify_app.todo_list.id
-  branch_name = "main"
-  stage     = "PRODUCTION"
+resource "aws_s3_object" "todo_list_index" {
+  bucket = aws_s3_bucket.todo_list.bucket
+  key     = "index.html"
+  source = "./www/index.html"
+  etag = filemd5("./www/index.html")
+  content_type = "text/html"
+}
+
+resource "aws_s3_object" "todo_list_script" {
+  bucket = aws_s3_bucket.todo_list.bucket
+  key     = "script.js"
+  source = "./www/script.js"
+  etag = filemd5("./www/script.js")
 }
