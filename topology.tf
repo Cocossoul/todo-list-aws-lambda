6371e738f9f3d2428b2cb671d599947a486e9769
Corentin Pape <corentin.pape@epita.fr>
2023-01-15 19:00:57 +0100

    fix(terraform): add github creds and auto branch creation

--- topology.tf
+++ topology.tf
@@ -11,15 +11,27 @@ provider "aws" {
   region = "eu-west-3"
 }
 
+variable "github_access_token" {
+  type    = string
+}
+
 resource "aws_amplify_app" "todo_list" {
   name       = "todo-list"
   repository = "https://github.com/Cocossoul/todo-list-aws-lambda"
 
-  build_spec = file("amplify.yml")
+  access_token = var.github_access_token
 
+  enable_branch_auto_build = true
+
+  build_spec = file("amplify.yml")
   environment_variables = {
     AMPLIFY_MONOREPO_APP_ROOT = "www"
     AMPLIFY_DIFF_DEPLOY = "false"
   }
 }
 
+resource "aws_amplify_branch" "todo_list_prod" {
+  app_id      = aws_amplify_app.todo_list.id
+  branch_name = "main"
+  stage     = "PRODUCTION"
+}
